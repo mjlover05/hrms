64492c91a08d486daef096f67e2b27293669b850
mjlover05 <93177731+mjlover05@users.noreply.github.com>
2025-07-02 14:56:03 +0530

    Update SecurityConfig.java

--- src/main/java/com/hrms/config/SecurityConfig.java
+++ src/main/java/com/hrms/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login", "/apply", "/apply-form","/register-hr", "/submit-application", "/favicon.ico", "/css/**", "/js/**").permitAll()
                         .requestMatchers("/hr/**").hasRole("HR")
